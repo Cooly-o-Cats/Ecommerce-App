897d9fd36a1425027709d76663cb39133188e431
Liamr <liamreitsma@icloud.com>
2021-10-06 21:50:49 -0500

    Finshed of Main Design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <Product/>;
+  return <Home/>;
 };
 
 export default App;
